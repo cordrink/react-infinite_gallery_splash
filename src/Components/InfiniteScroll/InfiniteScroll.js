f0728b34873f1c0d87917e317e93564b87255f45
cordrink <masodavictorjudicael@gmail.com>
2024-09-25 10:39:26 +0100

    Ajout de la fonctionnalite de recherche

--- src/Components/InfiniteScroll/InfiniteScroll.js
+++ src/Components/InfiniteScroll/InfiniteScroll.js
@@ -6,10 +6,11 @@ export default function InfiniteScroll() {
   const [dataImg, setDataImg] = useState([[], [], []]);
   const [pageIndex, setPageIndex] = useState(1);
   const [searchState, setSearchState] = useState("random");
+  const [firstCall, setFirstCall] = useState(true);
 
   const infiniteFetchData = () => {
     fetch(
-      `https://api.unsplash.com/search/photos?page=${pageIndex}&per_page=30&query=${searchState}&client_id=ACCES_KEY_UNSPLASH`
+      `https://api.unsplash.com/search/photos?page=${pageIndex}&per_page=30&query=${searchState}&client_id=ACCESS_KEY_UNSPLASH`
     )
       .then((reponse) => reponse.json())
       .then((data) => {
@@ -34,18 +35,45 @@ export default function InfiniteScroll() {
         }
 
         setDataImg(newFreshState);
+        setFirstCall(false);
+      });
+  };
+
+  const searchFetchData = () => {
+    fetch(
+      `https://api.unsplash.com/search/photos?page=${pageIndex}&per_page=30&query=${searchState}&client_id=ACCESS_KEY_UNSPLASH`
+    )
+      .then((reponse) => reponse.json())
+      .then((data) => {
+        const imgsReceived = [];
+
+        data.results.forEach((img) => {
+          imgsReceived.push(img.urls.regular);
+        });
+
+        const newFreshState = [[], [], []];
+
+        let index = 0;
+        for (let i = 0; i < 3; i++) {
+          for (let j = 0; j < 10; j++) {
+            newFreshState[i].push(imgsReceived[index]);
+            index++;
+          }
+        }
+
+        setDataImg(newFreshState);
       });
   };
 
   useEffect(() => {
+    if (firstCall) return;
+
+    searchFetchData();
+  }, [searchState]);
+
+  useEffect(() => {
     infiniteFetchData();
   }, [pageIndex]);
-
-  const handleSearch = (e) => {
-    e.preventDefault();
-  };
-
-  const inpRef = useRef();
 
   useEffect(() => {
     window.addEventListener("scroll", infiniteCheck);
@@ -53,6 +81,15 @@ export default function InfiniteScroll() {
       window.removeEventListener("scroll", infiniteCheck);
     };
   }, []);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+
+    setSearchState(inpRef.current.value);
+    setPageIndex(1);
+  };
+
+  const inpRef = useRef();
 
   const infiniteCheck = () => {
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
@@ -72,17 +109,17 @@ export default function InfiniteScroll() {
       <div className="card-list">
         <div>
           {dataImg[0].map((img) => {
-            return <img key={uuidv4} src={img} alt="img unsplash" />;
+            return <img key={uuidv4()} src={img} alt="img unsplash" />;
           })}
         </div>
         <div>
           {dataImg[1].map((img) => {
-            return <img key={uuidv4} src={img} alt="img unsplash" />;
+            return <img key={uuidv4()} src={img} alt="img unsplash" />;
           })}
         </div>
         <div>
           {dataImg[2].map((img) => {
-            return <img key={uuidv4} src={img} alt="img unsplash" />;
+            return <img key={uuidv4()} src={img} alt="img unsplash" />;
           })}
         </div>
       </div>
